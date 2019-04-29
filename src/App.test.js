b785dc8ed2b84ad852c50c158b49ea2858e31e29
ghost0fharambe <43300063+ghost0fharambe@users.noreply.github.com>
2019-04-28 20:49:54 -0700

    Updates

--- src/App.test.js
+++
@@ -1,5 +0,0 @@
-import React, { Component } from "react";
-import FriendCard from "./components/FriendCard";
-import Wrapper from "./components/Wrapper";
-import Title from "./components/Title";
-import friends from "./friends.json";
--- src/components/LossFlash/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./style.css";
-
-function LossFlash(props) {
-	return <div className="alert alert-danger" role="alert">Game Over!</div>
-};
-
-export default LossFlash;
