9bd8523b80085bdba26c91659b0e18729fac3423
ghost0fharambe <43300063+ghost0fharambe@users.noreply.github.com>
2019-05-28 13:50:38 -0700

    Updates

---
+++ precache-manifest.655086c2b8083059a3bbf932a4742f0b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fb1e7133362cdc4f7446",
+    "url": "/react-memory-game_redux/static/js/runtime~main.fb1e7133.js"
+  },
+  {
+    "revision": "619891c5095b6ea79217",
+    "url": "/react-memory-game_redux/static/js/main.619891c5.chunk.js"
+  },
+  {
+    "revision": "00ec0959a47ed983999e",
+    "url": "/react-memory-game_redux/static/js/2.00ec0959.chunk.js"
+  },
+  {
+    "revision": "619891c5095b6ea79217",
+    "url": "/react-memory-game_redux/static/css/main.eeb338f2.chunk.css"
+  },
+  {
+    "revision": "c95079730450ea628e18daec050a7be0",
+    "url": "/react-memory-game_redux/index.html"
+  }
+];
--- src/components/WinFlash/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./style.css";
-
-function WinFlash(props) {
-	return <div className="alert alert-success">You Win!</div>
-};
-
-export default WinFlash;
